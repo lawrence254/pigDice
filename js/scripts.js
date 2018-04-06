723f6f0c372e6f2877bd230a820b11443beab214
Lawrence <lawrenslava1@gmail.com>
2018-04-06 16:31:15 +0300

    Add ability for player to hold. Hide player 2's play button until player one gets a 1 or holds.

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,9 @@ Player.prototype.roll = function(roll) {
   this.total = this.total + roll;
   return this.total;
 }
+function gameOver(player){
+  alert("Game Over!"+player);
+}
 $(document).ready(function() {
   var rollCount = [];
   var players = [];
@@ -18,10 +21,17 @@ $(document).ready(function() {
     var rollNumber = Math.floor(Math.random() * 6) + 1;
     if (rollNumber === 1) {
       alert("Sorry...You rolled a one my niggah....try your luck next time");
+      player1.roll(0);
+      $('#rollDie2').toggle();
+      $('#rollDie1').toggle();
     }else {
       rollCount.push(rollNumber);
       player1.roll(rollNumber);
-      console.log("PLayer 1 Total is:"+player1.total);
+      if (player1.total >= 100) {
+        var player = "PLayer 1";
+        gameOver(player);
+      }
+      console.log("Player 1 Total is:"+player1.total);
       console.log(rollCount);
     }
     console.log(rollNumber);
@@ -31,13 +41,28 @@ $(document).ready(function() {
     var rollNumber = Math.floor(Math.random() * 6) + 1;
     if (rollNumber === 1) {
       alert("Sorry...You rolled a one my niggah....try your luck next time");
+      player2.roll(0);
+      $('#rollDie2').toggle();
+      $('#rollDie1').toggle();
     }else {
       rollCount.push(rollNumber);
       player2.roll(rollNumber);
-      console.log("PLayer 2 Total is:"+player2.total);
+      console.log("Player 2 Total is:"+player2.total);
       console.log(rollCount);
     }
-    console.log(rollNumber);
+
+  });
+  $('#hold').click(function (event) {
+    event.preventDefault();
+    if ($('#rollDie1').is(':visible')) {
+      alert("You chose to hold. Your score is: "+player1.total);
+      $('#rollDie2').toggle();
+      $('#rollDie1').toggle();
+    }else {
+      alert("You chose to hold. Your score is: "+player2.total);
+      $('#rollDie2').toggle();
+      $('#rollDie1').toggle();
+    }
   });
 
 });
