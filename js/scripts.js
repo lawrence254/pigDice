869401da39340e4fc76a593da668247c2afd7998
Lawrence <lawrenslava1@gmail.com>
2018-04-07 22:06:50 +0300

    Display users score on the html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function Player(name) {
   this.name = name;
-  this.score = 0;
+  // this.score = 0;
   this.total = 0;
 }
 
@@ -14,24 +14,39 @@ function gameOver(player){
 $(document).ready(function() {
   var rollCount = [];
   var players = [];
-  var player1 = new Player(name);
-  var player2 = new Player(name);
+  var player1 = {};
+  var player2 = {};
+
+  $('form#rollDice').submit(function (event) {
+    event.preventDefault();
+    var p1= $('input#player1').val()
+    var p2= $('input#player2').val()
+
+    player1 = new Player(p1);
+    player2 = new Player(p2);
+
+    console.log("Player 1 is: "+player1.name+" And Player 2 is: "+player2.name);
+  });
   $('#rollDie1').click(function(event){
     event.preventDefault();
     var rollNumber = Math.floor(Math.random() * 6) + 1;
     if (rollNumber === 1) {
-      alert("Sorry...You rolled a one my niggah....try your luck next time");
+      // alert("Sorry...You rolled a one my niggah....try your luck next time");
       player1.roll(0);
+      $('#player1Score').empty();
+      $('#player1Score').append("You rolled a 1: "+player1.total);
       $('#rollDie2').toggle();
       $('#rollDie1').toggle();
     }else {
       rollCount.push(rollNumber);
       player1.roll(rollNumber);
       if (player1.total >= 100) {
-        var player = "PLayer 1";
+        var player = player.name;
         gameOver(player);
       }
-      console.log("Player 1 Total is:"+player1.total);
+      $('#player1Score').empty();
+      $('#player1Score').append("Your Total is: "+player1.total);
+      // console.log(player1.name+" Total is:"+player1.total);
       console.log(rollCount);
     }
     console.log(rollNumber);
@@ -40,14 +55,24 @@ $(document).ready(function() {
     event.preventDefault();
     var rollNumber = Math.floor(Math.random() * 6) + 1;
     if (rollNumber === 1) {
-      alert("Sorry...You rolled a one my niggah....try your luck next time");
+      // alert("Sorry...You rolled a one my niggah....try your luck next time");
+
       player2.roll(0);
+      $('#player2Score').empty();
+      $('#player2Score').append("You rolled a 1: "+player2.total);
       $('#rollDie2').toggle();
+      $('#player1Score').empty();
       $('#rollDie1').toggle();
     }else {
       rollCount.push(rollNumber);
       player2.roll(rollNumber);
-      console.log("Player 2 Total is:"+player2.total);
+      if (player1.total >= 100) {
+        var player = player.name;
+        gameOver(player);
+      }
+      $('#player2Score').empty();
+      $('#player2Score').append("Your Total is: "+player2.total);
+      // console.log(player2.name+" Total is:"+player2.total);
       console.log(rollCount);
     }
 
@@ -55,11 +80,14 @@ $(document).ready(function() {
   $('#hold').click(function (event) {
     event.preventDefault();
     if ($('#rollDie1').is(':visible')) {
-      alert("You chose to hold. Your score is: "+player1.total);
+      $('#player1Score').empty();
+      $('#player1Score').append("You chose to hold. Your score is: "+player1.total);
       $('#rollDie2').toggle();
       $('#rollDie1').toggle();
     }else {
-      alert("You chose to hold. Your score is: "+player2.total);
+      $('#player2Score').empty();
+      $('#player2Score').append("You chose to hold. Your score is: "+player2.total);
+      // alert("You chose to hold. Your score is: "+player2.total);
       $('#rollDie2').toggle();
       $('#rollDie1').toggle();
     }
