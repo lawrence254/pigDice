d39fd9922d452949a8fe78c2041bb42f38b93889
Lawrence <lawrenslava1@gmail.com>
2018-04-10 11:38:38 +0300

    Overhaul all of the code. Ensure the game works as expected

--- js/scripts.js
+++ js/scripts.js
@@ -1,92 +1,86 @@
+var player1 = "";
+var player2 = "";
+
 function Player(name) {
   this.name = name;
-  // this.score = 0;
   this.total = 0;
+  this.diceFace = 0;
+  this.score = 0;
 }
 
-Player.prototype.roll = function(roll) {
-  this.total = this.total + roll;
-  return this.total;
+function diceThrow() {
+  return Math.floor(6 * Math.random()) + 1;
 }
-function gameOver(player){
-  alert("Game Over! "+ player +" Wins!!!!!");
+
+Player.prototype.rollAOne = function() {
+  if (this.diceFace === 1) {
+    this.score = 0;
+    $('#rollDie2').toggle();
+    $('#rollDie1').toggle();
+  } else {
+    this.score += this.diceFace;
+  }
+}
+
+Player.prototype.hold = function() {
+  this.total += this.score;
+  this.score = 0;
+}
+
+Player.prototype.winner = function() {
+  if (this.total >= 100) {
+    alert("Congratulations " + this.name + " You have won!!");
+  }
 }
 $(document).ready(function() {
-  var rollCount = [];
-  var players = [];
-  var player1 = {};
-  var player2 = {};
-
-  $('form#rollDice').submit(function (event) {
+  $('form#rollDice').submit(function(event) {
     event.preventDefault();
-    var p1= $('input#player1').val()
-    var p2= $('input#player2').val()
+    var p1 = $('input#player1').val()
+    var p2 = $('input#player2').val()
 
     player1 = new Player(p1);
-     player2 = new Player(p2);
+    player2 = new Player(p2);
 
-    console.log("Player 1 is: "+player1.name+" And Player 2 is: "+player2.name);
+    console.log("Player 1 is: " + player1.name + " And Player 2 is: " + player2.name);
   });
-  $('#rollDie1').click(function(event){
+  $('#rollDie1').click(function(event) {
     event.preventDefault();
-    var rollNumber = Math.floor(Math.random() * 6) + 1;
-    if (rollNumber === 1) {
-      // alert("Sorry...You rolled a one my niggah....try your luck next time");
-      player1.roll(0);
-      $('#player1Score').empty()
-      $('#player1Score').append("You rolled a 1: "+player1.total);
-      $('#rollDie2').toggle();
-      $('#rollDie1').toggle();
-    }else {
-      rollCount.push(rollNumber);
-      player1.roll(rollNumber);
-      if (player1.total >= 100) {
-        var player = player1.name;
-        gameOver(player);
-      }
-      $('#player1Score').empty();
-      $('#player1Score').append("Your Total is: "+player1.total);
-      // console.log(player1.name+" Total is:"+player1.total);
-      console.log(rollCount);
-    }
-    console.log(rollNumber);
+    player1.diceFace = diceThrow();
+    $('#player1Score').empty()
+    $('#roll1Total').empty();
+    $('#player1Score').append("You rolled a: " + player1.diceFace);
+    player1.rollAOne();
+    $('#roll1Total').append("Current total is: " + player1.score);
   });
-  $('#rollDie2').click(function(event){
+  $('#rollDie2').click(function(event) {
     event.preventDefault();
-    var rollNumber = Math.floor(Math.random() * 6) + 1;
-    if (rollNumber === 1) {
-      alert("Sorry...You rolled a one my niggah....try your luck next time");
-      player2.roll(0);
-      $('#rollDie2').toggle();
-      $('#rollDie1').toggle();
-    }else {
-      rollCount.push(rollNumber);
-      player2.roll(rollNumber);
-      if (player1.total >= 100) {
-        var player = player2.name;
-        gameOver(player);
-      }
-      $('#player2Score').empty();
-      $('#player2Score').append("Your Total is: "+player2.total);
-      // console.log(player2.name+" Total is:"+player2.total);
-      console.log(rollCount);
-    }
-
+    player2.diceFace = diceThrow();
+    $('#player2Score').empty()
+    $('#roll2Total').empty();
+    $('#player2Score').append("You rolled a: " + player2.diceFace);
+    player2.rollAOne();
+    $('#roll2Total').append("Current total is: " + player2.score);
   });
-  $('#hold').click(function (event) {
+  $("#hold").click(function(event) {
     event.preventDefault();
     if ($('#rollDie1').is(':visible')) {
+      player1.hold();
       $('#player1Score').empty();
-      $('#player1Score').append("You chose to hold. Your score is: "+player1.total);
+      $('#player1Final').empty();
+      $('#player1Final').append("Score for "+player1.name+"is: " + player1.total);
+      $(".roll1Total").empty();
       $('#rollDie2').toggle();
       $('#rollDie1').toggle();
-    }else {
+      player1.winner();
+    } else {
+      player2.hold();
       $('#player2Score').empty();
-      $('#player2Score').append("You chose to hold. Your score is: "+player2.total);
-      // alert("You chose to hold. Your score is: "+player2.total);
+      $('#player2Final').empty();
+      $('#player2Final').append("Score for "+player2.name+"is: " + player2.total);
+      $(".roll1Total").empty();
       $('#rollDie2').toggle();
       $('#rollDie1').toggle();
+      player2.winner();
     }
   });
-
 });
